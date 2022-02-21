3106365bb37a35e5ace0f52206dc9729148464d5
kyosuke <doradora8900@gmail.com>
2022-02-22 07:06:53 +0900

    Count App done

--- pages/countapp.vue
+++ pages/countapp.vue
@@ -5,13 +5,21 @@
       <h2>
         Count Apps
       </h2>
-      <h1>
+      <p id="increase_count">
         0
-      </h1>
-      <button class="btn">
+      </p>
+      <button class="btn" @click="increase()">
         Increase Namber
       </button>
-      
+      <button class="btn" @click="save()">
+        Save Namber
+      </button>
+      <button class="btn" @click="reset()">
+        Reset Namber
+      </button>
+      <p id="save_count">
+         last Namber is 
+      </p>
     </div>
     <div>
 
@@ -20,6 +28,28 @@
 </template>
 <script>
 export default {
+  data(){
+    return{
+      count:0,
+      
+    };
+  },
+  methods:{
+    increase(){
+      this.count += 1;
+      document.getElementById("increase_count").innerText = this.count
+    },
+    save(){
+      document.getElementById("save_count").innerText = document.getElementById("save_count").innerText +" "+ this.count + " - "
+      this.count = 0;
+      document.getElementById("increase_count").innerText = this.count
+    },
+    reset(){
+      this.count = 0;
+      document.getElementById("increase_count").innerText = this.count
+      document.getElementById("save_count").innerText = "last Namber is "
+    }
+  }
 
 }
 </script>
@@ -27,11 +57,11 @@ export default {
 .Apps{
   text-align: center;
 }
-.Apps h1{
+.Apps p{
   margin: 100px auto;
+  font-size: 30px;
 }
 .btn{
-
   padding: 20px 30px;
   background-color: black;
   color: white;
